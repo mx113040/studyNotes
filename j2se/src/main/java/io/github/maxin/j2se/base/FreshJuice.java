9d02cc961fe322c77a8467fa56d7e90c02eef66c
maxin <mx113040@gmail.com>
2018-06-16 18:04:58 +0800

    commit script  about enum example

---
+++ j2se/src/main/java/io/github/maxin/j2se/base/FreshJuice.java
@@ -0,0 +1,6 @@
+package io.github.maxin.j2se.base;
+
+public class FreshJuice {
+    enum FreshJuiceSize{SMALL,MEDIUM,LARGE};
+    FreshJuiceSize size;
+}
---
+++ j2se/src/main/java/io/github/maxin/j2se/base/FreshJuiceTest.java
@@ -0,0 +1,8 @@
+package io.github.maxin.j2se.base;
+
+public class FreshJuiceTest {
+    public static void main(String[] args) {
+        FreshJuice juice=new FreshJuice();
+        juice.size=FreshJuice.FreshJuiceSize.MEDIUM;
+    }
+}
